59b28b605c1733271e83a722e9587b868e6c0963
Jairo <jmomcontato@gmail.com>
2022-01-04 14:42:14 -0300

    :sparkles: defines a sample reader

---
+++ custom_readers/date_hierarchy_reader.py
@@ -0,0 +1,27 @@
+from typing import Tuple, Union
+
+from base_reader import BaseReader
+
+
+class DateHierarchyReader(BaseReader):
+    def __init__(self, path: str) -> None:
+        super().__init__(path)
+        self.file_pointer = open(path, "r")
+        self.isFirstLine = True
+
+    def readline(self) -> Union[str, None]:
+        if self.isFirstLine:
+            # tossing header away
+            self.file_pointer.readline()
+        line = self.file_pointer.readline()
+        if not line:
+            return None
+        identifier, dia, mes = line.replace('"', "").split(",")
+        dia = str(int(float(dia)))
+        mes = str(int(float(mes)))
+        if int(dia) < 10:
+            dia = "0" + dia
+        if int(mes) < 10:
+            mes = "0" + mes
+        subdirectory = f"{mes}/{dia}/{identifier}/"
+        return subdirectory
